210e34a4556eaa22ced2a016a9f36412448768ea
aatish.kumar <aatish.kumar@appinventiv.com>
2023-12-22 08:45:43 +0530

    minor change

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
   return (
     <nav className='fixed top-0 w-full h-20 z-[49] bg-[#252731] lg:px-4 flex justify-between items-center shadow-sm'>
       <Logo />
-      <Search />
+      {/* <Search searchParams="true" /> */}
     </nav>
   );
 };
